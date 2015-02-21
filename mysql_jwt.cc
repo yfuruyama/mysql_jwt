11dd92e658596e2df7a5772c587814eaa6509438
Yuuki Furuyama <addsict@gmail.com>
2015-02-21 18:48:04 +0900

    handle error on mysql_jwt.cc

--- mysql_jwt.cc
+++ mysql_jwt.cc
@@ -24,7 +24,6 @@ std::string get_claim_part(std::string jwt)
   std::getline(ss, header, '.');
 
   std::getline(ss, claim, '.');
-
   return claim;
 }
 
@@ -34,9 +33,16 @@ int format_pretty(picojson::object obj, std::string *out)
 
   const char* keys[] = {"iss", "sub", "aud", "exp", "iat", "jti"};
   for (int i = 0, l = sizeof(keys) / sizeof(char*); i < l; ++i) {
-    char buf[1024];
-    sprintf(buf, "    \"%s\": %s\n", keys[i], obj[keys[i]].to_str().c_str());
-    (*out) += buf;
+    const char* key = keys[i];
+    if (!obj[key].is<picojson::null>()) {
+      char buf[1024];
+      if (obj[key].is<std::string>()) {
+        sprintf(buf, "    \"%s\": \"%s\",\n", key, obj[key].to_str().c_str());
+      } else {
+        sprintf(buf, "    \"%s\": %s,\n", key, obj[key].to_str().c_str());
+      }
+      (*out) += buf;
+    }
   }
 
   (*out) += "}";
@@ -46,6 +52,15 @@ int format_pretty(picojson::object obj, std::string *out)
 
 my_bool decode_jwt_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
 {
+  if (args->arg_count != 1) {
+    strcpy(message, "Incorrect parameter count in the call to function 'decode_jwt'");
+    return 1;
+  }
+  if (args->arg_type[0] != STRING_RESULT) {
+    strcpy(message, "1st argument of 'decode_jwt' should be string");
+    return 1;
+  }
+
   initid->const_item = 0;
   initid->maybe_null = 1;
 
@@ -58,25 +73,37 @@ void decode_jwt_deinit(UDF_INIT *initid)
 
 char *decode_jwt(UDF_INIT *initid, UDF_ARGS *args,
                  char *result, unsigned long *res_length,
-                 char *null_value, char *error)
+                 char *is_null, char *error)
 {
   std::string jwt = (char*)args->args[0];
+
   const std::string claim_part = get_claim_part(jwt);
+  if (claim_part.empty()) {
+    *is_null = 1;
+    return NULL;
+  }
 
   std::string claim;
-  if (Base64::Decode(claim_part, &claim)) {
-    picojson::value v;
-    std::string err = picojson::parse(v, claim);
-    if (!err.empty()) {
-    } else {
-      picojson::object obj = v.get<picojson::object>();
-      std::string out; 
-      int out_len = format_pretty(obj, &out);
-
-      strcpy(result, out.c_str());
-      *res_length = out_len;
-    }
+  bool is_success = Base64::Decode(claim_part, &claim);
+  if (!is_success) {
+    *is_null = 1;
+    return NULL;
   }
+
+  picojson::value v;
+  std::string err = picojson::parse(v, claim);
+  if (!err.empty()) {
+    *is_null = 1;
+    return NULL;
+  }
+
+  picojson::object obj = v.get<picojson::object>();
+  std::string out; 
+  int out_len = format_pretty(obj, &out);
+
+  strcpy(result, out.c_str());
+  *res_length = out_len;
 
   return result;
 }
+
